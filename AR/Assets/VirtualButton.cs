024272781de46ba853601842d4bea1665f46c538
howardlau <howardlau1999@hotmail.com>
2019-12-22 18:51:00 +0800

    AR

---
+++ AR/Assets/VirtualButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Vuforia;
+public class VirtualButton :  MonoBehaviour
+{
+    public GameObject vb, solid, dragon;
+    public Animator ani;
+    VirtualButtonBehaviour behaviour;
+    public Material pressed, normal;
+    void Awake()
+    {
+        behaviour = vb.GetComponent<VirtualButtonBehaviour>();
+        Debug.Log(behaviour);
+        if (behaviour)
+        {
+            behaviour.RegisterOnButtonReleased(OnButtonReleased);
+            behaviour.RegisterOnButtonPressed(OnButtonPressed);
+        }
+        ani = dragon.GetComponent<Animator>();
+    }
+
+    void Destroy()
+    {
+        behaviour = vb.GetComponent<VirtualButtonBehaviour>();
+        if (behaviour)
+        {
+            behaviour.UnregisterOnButtonReleased(OnButtonReleased);
+            behaviour.UnregisterOnButtonPressed(OnButtonPressed);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonPressed: " + vb.VirtualButtonName);
+        solid.GetComponent<MeshRenderer>().sharedMaterial = pressed;
+        ani.SetTrigger("Take Off");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonReleased: " + vb.VirtualButtonName);
+        solid.GetComponent<MeshRenderer>().sharedMaterial = normal;
+        ani.SetTrigger("Land");
+    }
+}
